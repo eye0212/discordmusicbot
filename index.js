aaef0136f83e32b180fe4a1cf8745db23537eeed
eug0212 <43792940+eug0212@users.noreply.github.com>
2022-01-12 21:39:04 -0500

    Trying this again with procfile

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ client.on("message", async message => {
 
   const serverQueue = queue.get(message.guild.id);
 
-  if (message.content.startsWith(`${prefix}p`)) {
+  if (message.content.startsWith(`${prefix}play`)) {
     execute(message, serverQueue);
     return;
   } else if (message.content.startsWith(`${prefix}skip`)) {
@@ -101,11 +101,11 @@ function skip(message, serverQueue) {
 function stop(message, serverQueue) {
   if (!message.member.voice.channel)
     return message.channel.send(
-      "ya mama said stop last night"
+      "You have to be in a voice channel to stop the music!"
     );
     
   if (!serverQueue)
-    return message.channel.send("ya mama said stop last night");
+    return message.channel.send("There is no song that I could stop!");
     
   serverQueue.songs = [];
   serverQueue.connection.dispatcher.end();
